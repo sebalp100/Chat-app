b01988dd9f40b5e25b876cfdce655c6cae5cdbe1
sebalp100 <sebalp100@gmail.com>
2023-01-17 00:48:17 -0300

    Add search user functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,99 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Search = () => {
+  const [username, setUsername] = useState('');
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, 'users'),
+      where('displayName', '==', username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === 'Enter' && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, 'chats', combinedId));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, 'chats', combinedId), { messages: [] });
+
+        await updateDoc(doc(db, 'userChats', currentUser.uid), {
+          [combinedId + '.userInfo']: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + '.date']: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, 'userChats', user.uid), {
+          [combinedId + '.userInfo']: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + '.date']: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
+
+    setUser(null);
+    setUsername('');
+  };
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='Find a user' />
-      </div>
-      <div className='userChat'>
-        <img
-          src='https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg'
-          alt=''
+        <input
+          type='text'
+          placeholder='Find a user'
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
-        <div className='userChatInfo'>
-          <span>Other</span>
-        </div>
       </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className='userChat' onClick={handleSelect}>
+          <img src={user.photoURL} alt='' />
+          <div className='userChatInfo'>
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
